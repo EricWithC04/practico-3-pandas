2ef9a5d9b43378cae3299bd40fd0806facecc027
EricWithC04 <negativegame117@gmail.com>
2024-04-18 21:13:33 -0300

    feat: control exceptions

--- app.py
+++ app.py
@@ -1,24 +1,42 @@
 import pandas as pd
 
 def analisis_estadistico(l):
-    l.sort()
+    try:
 
-    fi = {}
-    for i in l:
-        if i in fi:
-            fi[i] += 1
-        else:
-            fi[i] = 1
-    
-    df = pd.DataFrame({'edades':fi.keys(),'fi':fi.values()})
+        if type(l) != list:
+            raise TypeError("El parametro recibido no es una lista")
 
-    df["Fi"] = df["fi"].cumsum()
-    df["ri"] = df["fi"] / df["fi"].sum()
-    df["Ri"] = df["ri"].cumsum()
-    df["pi%"] = df["ri"] * 100
-    df["Pi%"] = df["Ri"] * 100
+        if len(l) == 0:
+            raise ValueError("La lista está vacia")
+        
+        for element in l:
+            if type(element) != int:
+                raise TypeError("La lista contiene elementos no numericos")
 
-    return df
+        l.sort()
+
+        fi = {}
+        for i in l:
+            if i in fi:
+                fi[i] += 1
+            else:
+                fi[i] = 1
+
+        df = pd.DataFrame({'edades':fi.keys(),'fi':fi.values()})
+
+        df["Fi"] = df["fi"].cumsum()
+        df["ri"] = df["fi"] / df["fi"].sum()
+        df["Ri"] = df["ri"].cumsum()
+        df["pi%"] = df["ri"] * 100
+        df["Pi%"] = df["Ri"] * 100
+
+        return df
+    except ValueError as e:
+        print(e)
+        return None
+    except TypeError as e:
+        print(e)
+        return None
 
 edades = [19, 29, 19, 22, 23, 19, 30, 19, 19, 19, 20, 20, 20, 18, 22, 19, 34, 34, 21, 21, 22, 28, 29, 19, 20, 19, 25, 28, 21, 22]
 
